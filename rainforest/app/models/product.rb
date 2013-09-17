08e4ff189412439ba0f3aaf229e5b5d431558b1e
hippli <philip.e.harrison@gmail.com>
2013-09-17 16:29:08 -0400

    Added review model and associations

--- rainforest/app/models/product.rb
+++ rainforest/app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ActiveRecord::Base
 
+  has_many :reviews
+  has_many :users, :through => :reviews
+
   # attr_accessible :description, :name, :price_in_cents - not required in rails 4.0
 
   validates :description, :name, :presence => true
